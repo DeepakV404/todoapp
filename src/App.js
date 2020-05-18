5333b7787c9e5ac5d5597efcedda9e98d4de4852
Deepak <17deepakrv5@gmail.com>
2020-05-18 23:02:32 +0530

    Added onSubmit functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component{
+  constructor(props){
+    super(props);
+    
+    this.state={
+      items:[],
+      currentItem:{
+        text:"",
+        key:"",
+      
+      }
+    }
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+
+  }
+
+  handleInput(e){
+    this.setState({
+      currentItem:{
+        text:e.target.value,
+        key:Date.now(),
+      }
+    })
+  }
+
+  addItem(e){
+    e.preventDefault();
+    const newitem = this.state.currentItem;
+    console.log(newitem);
+  }
+
+  render(){
   return (
     <div className="App">
-      <header id = "to-do-form">
-        <input type="text" placeholder="Say something"></input>
-        <button type="submit">Add</button>
+      <header>
+        <form id = "to-do-form" onSubmit={this.addItem}>
+          <input type="text" placeholder="Say something" value={this.state.currentItem.text}
+          onChange={this.handleInput}/>
+          <button type="submit">Add</button>
+        </form>
       </header>
     </div>
   );
+}
 }
 
 export default App;
